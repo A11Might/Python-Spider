4496b13791594cc1b23063c6f7111dbaf26bb13d
Kohath Hu <qihanghu@foxmail.com>
2019-04-29 18:44:08 +0800

    Update Edge.java

--- learningCode/Edge.java
+++ learningCode/Edge.java
@@ -6,7 +6,7 @@ package class_6;
 public class Edge {
     public int weight;
     public Node from;
-	public Node to;
+    public Node to;
 
     public Edge(int weight, Node from, Node to) {
         this.weight = weight;
