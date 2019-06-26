504b6020ed4f4e79b73659b84431809788d7e3f5
Qihang Hu <qihanghu@foxmail.com>
2019-06-26 22:17:47 +0800

    Update AllLessNumSubArray.java

--- highClass/AllLessNumSubArray.java
+++ highClass/AllLessNumSubArray.java
@@ -3,7 +3,7 @@ package day.class02;
 import java.util.LinkedList;
 
 /**
- * 题目：给定数组arr和整数num，共返回有多少个子数组满足如下情况： 
+ * 题目：给定数组arr和整数num，返回共有多少个子数组满足如下情况： 
  * max(arr[i...j]) - min(arr[i...j]) <= num
  * max(arr[i...j]) 表示子数组arr[i...j]中的最大值，min(arr[i...j]) 表示子数组arr[i...j]中的最小值
  * 要求：如果数组长度为N,请实现时间复杂度为O(N)的解法
